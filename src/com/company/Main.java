95fa5e76bc2f407deeb0990606fc6b7953c49e4c
sleitor <Sleitor@list.ru>
2017-04-04 21:04:18 +0300

    More Elements. But first is gone.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,11 @@ public class Main {
         LinkedList linkedList = new LinkedList();
 //        linkedList.add("dfsd");
         System.out.println(linkedList.size() );
-        linkedList.add("dfsd");
+        linkedList.add("dfsd1");
+        linkedList.add("dfsd2");
+        linkedList.add("dfsd3");
         System.out.println(linkedList.size() );
-        System.out.println(linkedList.get(1).toString() );
+        System.out.println(linkedList.get(0) );
+        System.out.println(linkedList.get(1) );
     }
 }
