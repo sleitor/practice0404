c83e78788a2c8220d3660cf9eaa6f6170e743874
sleitor <Sleitor@list.ru>
2017-04-04 13:04:08 +0300

    Initial Commit#2

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -4,8 +4,15 @@ package com.company;
  * Created by admin on 04.04.2017.
  */
 public class Node {
+    private int Value;
+    private int IndexNext;
+    private int IndexPrev;
+    private int Head;
+    private int Tail;
 
     Node() {
 
     }
+
+
 }
